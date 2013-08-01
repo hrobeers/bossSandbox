823ff9d4be3120d98f35453990ec5f6510963914
hrobeers <hansrobeers@gmail.com>
2013-08-01 16:39:35 +0000

    About action

--- src/controller/bossSandbox_main_controller.erl
+++ src/controller/bossSandbox_main_controller.erl
@@ -3,7 +3,10 @@
 -compile(export_all).
 
 index('GET', []) ->
-    {ok, [{title, "bossSandbox"}]}.
+    {ok, [{title, "Home"}]}.
+
+about('GET',[]) ->
+    {ok, [{title, "About"}]}.
 
 nope('GET', []) ->
     {ok, []}.
